aa854b0778d84e453225ca99fbe086d38d6274f7
Michelle Teh <61931927+emichelle-teh@users.noreply.github.com>
2021-01-09 01:54:55 +0700

    Update classes.py
    trying to make a merge conflict

--- classes.py
+++ classes.py
@@ -2,13 +2,12 @@
 # self -> doing it on itself 
 # like me.yeet
 class Point():
-    def __init__(self, input1, input3):
+    def __init__(self, input1, input3, input5):
         self.x = input1
         self.y = input3
-        
-        #yayeet
+        self.z = input5
 
-p = Point(2,8)
+p = Point(2,8,9)
 print(p.x)
 print(p.y) 
 
