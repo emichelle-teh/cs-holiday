bb2f7e84b62b809994ae6f7847d978ac77330038
michelleteh <nmteh17@students.cs.ubc.ca>
2021-01-09 01:45:52 +0700

    Changed minor detail

--- classes.py
+++ classes.py
@@ -2,11 +2,12 @@
 # self -> doing it on itself 
 # like me.yeet
 class Point():
-    def __init__(self, input1, input2):
+    def __init__(self, input1, input3):
         self.x = input1
-        self.y = input2
+        self.y = input3
 
 p = Point(2,8)
 print(p.x)
 print(p.y) 
+
 
