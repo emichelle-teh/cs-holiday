9fd0b2b861755749fe7ec9acc5195d7ce3445b7d
michelleteh <nmteh17@students.cs.ubc.ca>
2021-01-09 01:38:56 +0700

    added classes.py

---
+++ classes.py
@@ -0,0 +1,12 @@
+# if you do init -> it's like initializing
+# self -> doing it on itself 
+# like me.yeet
+class Point():
+    def __init__(self, input1, input2):
+        self.x = input1
+        self.y = input2
+
+p = Point(2,8)
+print(p.x)
+print(p.y) 
+
